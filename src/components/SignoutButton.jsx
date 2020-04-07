e2cef1246643307521bfc8deeb8e98bbba9e7c6b
naoki-funawatari <waribasika@live.jp>
2020-04-07 21:12:00 +0900

    feat: サインアウト時の明示的な画面遷移を削除。

--- src/components/SignoutButton.jsx
+++ src/components/SignoutButton.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useDispatch } from "react-redux";
-import { useHistory } from 'react-router-dom';
 import { deleteToken } from '../stores/token';
 import { Button } from '@material-ui/core';
 
 export default () => {
   const dispatch = useDispatch();
-  const history = useHistory();
-  const signOut = () => {
-    dispatch(deleteToken());
-    history.push({ pathname: "/" });
-  }
+  const signOut = () => dispatch(deleteToken());
   return (
-    <Button variant="outlined" style={{ backgroundColor: 'white' }} onClick={signOut}>SIGN OUT</Button>
+    <Button
+      variant="outlined"
+      style={{ backgroundColor: 'white' }}
+      onClick={signOut}>
+      SIGN OUT
+    </Button>
   );
 }
