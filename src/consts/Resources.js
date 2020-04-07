886bab3557c2f1c264853a92d87b05ea54dee292
naoki-funawatari <waribasika@live.jp>
2020-04-07 23:27:25 +0900

    chore: ゴミ削除。

--- src/consts/Resources.js
+++
@@ -1,8 +0,0 @@
-export default [
-  { resourceId: 1, resourceTitle: '自分の予定' },
-  { resourceId: 2, resourceTitle: '会議室１' },
-  { resourceId: 3, resourceTitle: '会議室２' },
-  { resourceId: 4, resourceTitle: '会議室３' },
-  { resourceId: 5, resourceTitle: 'プロジェクター' },
-  { resourceId: 6, resourceTitle: 'ノートPC' },
-];
