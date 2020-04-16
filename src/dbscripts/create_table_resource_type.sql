52551902389c5acf10e1a9f8e4759cd3cf0e3021
naoki-funawatari <waribasika@live.jp>
2020-04-16 21:09:06 +0900

    feat: リソーステーブル定義変更。

--- src/dbscripts/create_table_resource_type.sql
+++ src/dbscripts/create_table_resource_type.sql
@@ -1,20 +1,17 @@
 USE [duty_times]
 GO
 
-/****** Object:  Table [dbo].[resources]    Script Date: 2020/04/16 20:49:44 ******/
+/****** Object:  Table [dbo].[resource_type]    Script Date: 2020/04/16 20:52:22 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[resources](
+CREATE TABLE [dbo].[resource_type](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[title] [nvarchar](20) NOT NULL,
-	[resource_type_id] [int] NOT NULL,
-	[remarks] [nvarchar](50) NULL,
-	[deleted] [datetime] NULL,
- CONSTRAINT [PK_resources] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_resource_type] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
