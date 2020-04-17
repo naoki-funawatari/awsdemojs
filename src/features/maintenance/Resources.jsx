44455a19a25d118f4066759a695858dbb4598638
naoki-funawatari <waribasika@live.jp>
2020-04-17 13:35:12 +0900

    feat: リソースの更新を可能に。（resource type は変更できない）

--- src/features/maintenance/Resources.jsx
+++ src/features/maintenance/Resources.jsx
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import { getResourcesAsync } from './resourcesSlice';
+import {
+  getResourcesAsync,
+  postResources, postResourcesAsync,
+  putResources, putResourcesAsync,
+  deleteResources, deleteResourcesAsync,
+} from './resourcesSlice';
 import MaterialTable from 'material-table';
 
+const columns = [
+  { title: 'Title', field: 'title' },
+  { title: 'ResourceType', field: 'resource_type_title' },
+  { title: 'Remarks', field: 'remarks' },
+];
 export default () => {
   const { resources } = useSelector(state => state);
   const dispatch = useDispatch();
@@ -10,62 +20,33 @@ export default () => {
     dispatch(getResourcesAsync());
   }, [dispatch]);
 
-  const [state, setState] = useState({});
-  useEffect(() => {
-    setState({
-      columns: [
-        { title: 'Title', field: 'title' },
-        { title: 'ResourceType', field: 'resource_type_title' },
-        { title: 'Remarks', field: 'remarks' },
-      ],
-      data: resources.map(resource => ({ ...resource })),
-    });
-    console.table(resources);
-    console.table([...resources]);
-    console.table(resources.map(resource => ({ ...resource })));
-  }, [resources]);
+  const handleRowAdd = async (newData) => {
+    await new Promise(resolve => setTimeout(resolve, 600));
+    console.log(newData)
+    dispatch(postResources(newData));
+    dispatch(postResourcesAsync(newData));
+  }
+  const handleRowUpdate = async (newData, oldData) => {
+    await new Promise(resolve => setTimeout(resolve, 600));
+    if (!oldData) return;
+    dispatch(putResources(newData));
+    dispatch(putResourcesAsync(newData));
+  }
+  const handleRowDelete = async (oldData) => {
+    await new Promise(resolve => setTimeout(resolve, 600));
+    dispatch(deleteResources(oldData));
+    dispatch(deleteResourcesAsync(oldData));
+  }
 
   return (
     <MaterialTable
-      title="Editable Example"
-      columns={state.columns}
-      data={state.data}
+      title="Editable Resources"
+      columns={columns}
+      data={resources.map(resource => ({ ...resource }))}
       editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
+        onRowAdd: handleRowAdd,
+        onRowUpdate: handleRowUpdate,
+        onRowDelete: handleRowDelete,
       }}
     />
   );
--- src/features/maintenance/resourcesSlice.js
+++ src/features/maintenance/resourcesSlice.js
@@ -8,13 +8,48 @@ const slice = createSlice({
   initialState,
   reducers: {
     updateResources: (state, action) => action.payload,
+    postResources: (state, action) => {
+      console.log('------------------postResources------------------');
+      const id = generateNewId(state);
+      const title = action.payload.title;
+      const resource_type_id = action.payload.resource_type_id;
+      const resource_type_title = action.payload.resource_type_title;
+      const remarks = action.payload.remarks;
+      const newResource = { id, title, resource_type_id, resource_type_title, remarks }
+      console.table(newResource);
+      return [...state, newResource];
+    },
+    putResources: (state, action) => {
+      console.log('------------------putResources------------------');
+      const id = action.payload.id;
+      const title = action.payload.title;
+      const resource_type_id = action.payload.resource_type_id;
+      const resource_type_title = action.payload.resource_type_title;
+      const remarks = action.payload.remarks;
+      const newResource = { id, title, resource_type_id, resource_type_title, remarks }
+      console.table(newResource);
+      const newResources = state.map(resource =>
+        resource.id === id
+          ? newResource
+          : resource
+      );
+      return newResources;
+    },
+    deleteResources: (state, action) => {
+      console.log('------------------deleteResources------------------');
+      const id = action.payload.id;
+      return state.filter(resource => resource.id !== id);
+    },
   },
 });
 
 export default slice.reducer;
 
 export const {
-  updateResources
+  updateResources,
+  postResources,
+  putResources,
+  deleteResources,
 } = slice.actions;
 
 export const getResourcesAsync = () => async dispatch => {
@@ -32,4 +67,25 @@ export const postResourcesAsync = resource => async dispatch => {
   } catch (error) {
     console.error(error);
   }
+}
+export const putResourcesAsync = resource => async dispatch => {
+  try {
+    const data = await fetchData('Resources', 'PUT', resource);
+    dispatch(updateResources(data));
+  } catch (error) {
+    console.error(error);
+  }
+}
+export const deleteResourcesAsync = resource => async dispatch => {
+  try {
+    const data = await fetchData('Resources', 'DELETE', resource);
+    dispatch(updateResources(data));
+  } catch (error) {
+    console.error(error);
+  }
+}
+export const generateNewId = resources => {
+  const ids = resources.map(resource => resource.id);
+  const newId = ids.length === 0 ? 1 : Math.max(...ids) + 1;
+  return newId;
 }
